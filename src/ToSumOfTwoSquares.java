cbbd226ee37fdcef760256e3ff771e9c27fc22e7
AndriyPyzh <andriy.pyzh2412@gmail.com>
2020-01-23 09:09:39 +0200

    Menu added

--- src/ToSumOfTwoSquares.java
+++ src/ToSumOfTwoSquares.java
@@ -7,13 +7,16 @@ import java.util.logging.Logger;
 public class ToSumOfTwoSquares {
 
     private static Logger log = Logger.getLogger(ToSumOfTwoSquares.class.getName());
+    private static Scanner input = new Scanner(System.in);
 
     public static void main(String... args) {
 
-        int n = 1885;
+//        int n = 1885;
+//
+//        log.info(Boolean.toString(existSumOfTwoSquares(n)));
+//        log.info(findSumOfTwoSquares(n).toString());
 
-        log.info(Boolean.toString(existSumOfTwoSquares(n)));
-        log.info(findSumOfTwoSquares(n).toString());
+        show();
 
     }
 
@@ -23,7 +26,7 @@ public class ToSumOfTwoSquares {
         for (int i = 0; i * i < m; i++) {
             for (int j = i; j * j <= m - i * i; j++) {
                 if (i * i + j * j == m) {
-                    pairs.add(Arrays.asList(i,j));
+                    pairs.add(Arrays.asList(i, j));
                 }
             }
         }
@@ -34,12 +37,12 @@ public class ToSumOfTwoSquares {
     public static boolean existSumOfTwoSquares(int m) {
         List<Integer> factors = factorizeNumber(m);
 
-        Set<Integer> uniqueFactors = new HashSet<Integer>(factors);
+        Set<Integer> uniqueFactors = new HashSet<>(factors);
 
         for (Integer i : uniqueFactors) {
             if (i % 4 == 3 && factors.stream()
-                              .filter(x -> x.equals(i))
-                              .count() % 2 != 0) {
+                    .filter(x -> x.equals(i))
+                    .count() % 2 != 0) {
 
                 return false;
             }
@@ -62,7 +65,55 @@ public class ToSumOfTwoSquares {
         return factors;
     }
 
+    public static void pressButton1() {
+        System.out.println("Input natural number m:");
+        int m = Integer.parseInt(input.nextLine());
 
+        boolean exist = existSumOfTwoSquares(m);
+        if (exist) {
+            System.out.format("Number %d can be represented as sum of two squares\n\n",m);
+        } else {
+            System.out.format("Number %d cannot be represented as sum of two squares\n\n",m);
+        }
+    }
+
+    public static void pressButton2() {
+        System.out.println("Input natural number m:");
+        int m = Integer.parseInt(input.nextLine());
+
+        List<List<Integer>> squares = findSumOfTwoSquares(m);
+        if (squares.isEmpty()) {
+            System.out.println("Number cannot be represented as sum of two squares\n");
+        } else {
+            for (List<Integer> pair : squares) {
+                System.out.format("x = %d, y = %d\n", pair.get(0), pair.get(1));
+            }
+        }
+    }
+
+    public static void outputMenu() {
+        System.out.println("1 - check if number can be represented as sum of two squares\n" +
+                "2 - find all x,y that x^2 + y^2 = n\n" +
+                "3 - EXIT");
+    }
+
+    public static void show() {
+        String keyMenu;
+        do {
+            System.out.println("Please, select menu point:");
+            outputMenu();
+
+            keyMenu = input.nextLine();
+
+            if (keyMenu.equals("1")) {
+                pressButton1();
+            } else if (keyMenu.equals("2")) {
+                pressButton2();
+            } else if (!keyMenu.equals("3"))
+                log.info("Invalid input");
+
+        } while (!keyMenu.equals("3"));
+    }
 
 }
 
